ab129ebdfa44961ba387b96f72b44988bd36a348
MultiApollo19 <mariusz.belwon@gmail.com>
2025-05-20 19:50:59 +0200

    next

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,25 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body className='bg-[#1b2038] font-roboto max-h-screen h-screen justify-between flex flex-col'>
-        <div className='bg-[#1b2038] w-full h-24 flex content-center flex-row'>
-          <Link href='/'>
-            <div className='h-16 w-52 flex mt-4 ml-6 hover:cursor-pointer '>
-              <Image src="/Assets/logo.svg" alt='logo' width={47} height={65} />
-              <div className='text-white text-4xl mt-3 ml-2'>SpaceLab</div>
-            </div>
-          </Link>
-          <div className='h-16 flex mt-8 ml-20 hover:cursor-pointer '>
-            <div className='text-white text-2xl'>Rockets</div>
-          </div>        
-        </div>
-        <div className='mb-auto max-h-screen '>{children}</div>
-        <div className='bg-[#1b2038] w-full h-24 flex flex-row justify-end'>
-          
-          <div className='text-white text-2xl mt-8 mr-10 text-opacity-40'>SpaceLab &copy; 2025</div>
-        </div>
-      </body>
+
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main >
-      <div className="flex justify-center flex-col place-items-center bg-[#252e50]">
-        <div className='mt-182 flex'>
-        </div>
-      </div>
-
-
     </main>
   );
 }
