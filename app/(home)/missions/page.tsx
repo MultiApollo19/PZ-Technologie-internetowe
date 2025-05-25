8cf3d3bab74b377b12813985d40c951f802884cd
MultiApollo19 <mariusz.belwon@gmail.com>
2025-05-25 21:29:11 +0200

    Card design

--- app/(home)/missions/page.tsx
+++ app/(home)/missions/page.tsx
@@ -4,19 +4,25 @@ import Card from "@/components/card";
 
 
 const getMisssions = unstable_cache(
-    async () => {return await prisma.missions.findMany()        
+    async () => {
+        return await prisma.missions.findMany()
     },
     [''],
-    {revalidate:60}
+    { revalidate: 60 }
 )
 
 export default async function missionsPage() {
     const missions = await getMisssions()
     return (
-        <div className="bg-white h-screen w-screen text-black grid grid-cols-7 gap-2 justify-items-center mt-10">
-            {missions.map((mission) => {
-                return <Card key={mission.id} {...mission} />;
-            })}
+        <div>
+            <div className="text-5xl font-bold text-center">
+                Missions
+            </div>
+            <div className="bg-white h-screen w-screen text-black grid grid-cols-6 justify-items-center mt-10 grid-rows-3 gap-y-50">
+                {missions.map((mission) => {
+                    return <Card key={mission.id} {...mission} />;
+                })}
+            </div>
         </div>
     )
 }
--- components/card.tsx
+++ components/card.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 interface Props{
     name:string | null;
     image:string | null;
@@ -8,10 +8,12 @@ interface Props{
 
 export default function Card({name,image,description_short}:Props) {
   return (
-    <div className="h-25 bg-">
-        {name}
-        <Image src={image ?? '/placeholder.png'} alt={name??"Mission not found"} width={128} height={128}/>
-        {description_short}
+    <Link href="">
+    <div className="w-64 border-1 shadow-2xl rounded-2xl items-center h-max p-2 min-h-90">
+        <div className="text-xl font-bold text-center">{name}</div>
+        <Image src={image ?? '/Images/placeholder.png'} alt={name??"Mission not found"} width={512} height={128} loading="lazy" className="w-52 h-52 rounded-3xl mx-auto" placeholder="blur" blurDataURL="/Images/blur.png"/>
+        <div className="h-12 mt-2 mx-auto px-2">{description_short}</div>
     </div>
+    </Link>
   );
 }
