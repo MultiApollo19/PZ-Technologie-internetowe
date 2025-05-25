dbc50faa1a49388ec52b09d5951db3e7fd147f03
MultiApollo19 <mariusz.belwon@gmail.com>
2025-05-25 20:32:33 +0200

    Vercel speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "SpaceLab",
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="relative">
+        <SpeedInsights/>
         <Analytics/>
         <Navbar/>
         {children}
